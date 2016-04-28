8b13451023fd05564252d9c142388e8a420da7c8
Lukasz Jaros <jaros.luk@gmail.com>
2016-04-28 23:12:22 +0200

    Added Calamari credentials to config

--- config.py
+++ config.py
@@ -12,3 +12,12 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 # Secret key for signing cookies
 SECRET_KEY = "secret"
+
+# Default Calamari API url
+CALAMARI_API_URL = "http://localhost/api/v2/"
+
+# Default Calamari API username
+CALAMARI_API_USER = "admin"
+
+# Default Calamari API password
+CALAMARI_API_PWD = "kujira"
