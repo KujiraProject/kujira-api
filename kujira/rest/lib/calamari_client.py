a73068b623aa16bcf7d43de55be1b7abb27eadaf
Lukasz Jaros <jaros.luk@gmail.com>
2016-04-28 22:56:56 +0200

    Added Calamari client implementation

---
+++ kujira/rest/lib/calamari_client.py
@@ -0,0 +1,28 @@
+import requests
+
+
+class CalamariClient(object):
+    def __init__(self, api_url, username, password):
+        self._api_url = api_url
+        self._username = username
+        self._password = password
+
+    def authenticate(self):
+        client = requests.session()
+        client.post(self._api_url+"auth/login/",
+                    {
+                        'username': self._username,
+                        'password': self._password
+                    })
+        print client.cookies
+        return client
+
+    def get(self, endpoint):
+        client = self.authenticate()
+        response = client.get(self._api_url+endpoint)
+        return response.json()
+
+    def post(self, endpoint, data):
+        client = self.authenticate()
+        response = client.post(self._api_url+endpoint, data=data)
+        return response.json()
