2f1e065400b126ed6ed4562e15f7e5585b23effb
Lukasz Jaros <jaros.luk@gmail.com>
2016-04-28 23:12:22 +0200

    Added Calamari credentials to config

---
+++ config.py
@@ -0,0 +1,23 @@
+# Statement for enabling the development environment
+DEBUG = True
+
+# Define the application directory
+import os
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+# Application threads. A common general assumption is
+# using 2 per available processor cores - to handle
+# incoming requests using one and performing background
+# operations using the other.
+
+# Secret key for signing cookies
+SECRET_KEY = "secret"
+
+# Default Calamari API url
+CALAMARI_API_URL = "http://localhost/api/v2/"
+
+# Default Calamari API username
+CALAMARI_API_USER = "admin"
+
+# Default Calamari API password
+CALAMARI_API_PWD = "kujira"
