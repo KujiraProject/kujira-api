1f4bb2567d085f74d948c71bc02b2c83ef71441a
vmyrhorodskyi <vmyrhorodskyi@gmail.com>
2016-05-19 08:46:44 +0200

    actualization of json api parsing methods

--- kujira/rest/controllers/clusters.py
+++ kujira/rest/controllers/clusters.py
@@ -2,40 +2,42 @@
 Methods mapped:
 - api/v2/clusters"""
 
-import logging
-
 from kujira.blueprints import CLUSTER_BP
 from kujira.rest.lib.request_methods import send_get
 from kujira.rest.lib.parsing_methods import parse_and_return
+from flask import Response
 
 
 @CLUSTER_BP.route("")
 def cluster():
     """Request for getting all clusters"""
     response = send_get('cluster')
-    if response.status_code != 422:
-        response = parse_and_return(clusters_parse, response)
+    if not isinstance(response, Response):
+        response = parse_and_return(parse_clusters, response)
     return response
 
 
-def clusters_parse(json_dict):
-    """Clusters parser to JSON API format"""
-    try:
-        new_dict = json_dict[0]
-    except KeyError as err:
-        new_dict = json_dict
-        logging.warning(str(err))
-    root = {'data': []}
+def parse_clusters(clusters):
+    """Function which parses clusters list or dict into JSON API format"""
+    result = {
+        'data': []
+    }
+    for cluster_dict in clusters:
+        current_cluster = parse_cluster(cluster_dict)
+    result['data'].append(current_cluster)
+    return result
+
+
+def parse_cluster(cluster_dict):
+    """Function which restructures cluster's dict entries into appropriate categories"""
+    result = {
+        'type': 'clusters'
+    }
     attributes = {}
-    if new_dict:
-        data = {'type': 'clusters'}
-        for key, value in new_dict.iteritems():
-            key = key.replace('_', '-')
-            if str(key) == 'name':
-                data['id'] = str(value)
-                attributes[key] = value
-            else:
-                attributes[key] = value
-        data['attributes'] = attributes
-    root['data'].append(data)
-    return root
+    for key, value in cluster_dict.iteritems():
+        key = key.replace('_', '-')
+        if str(key) == 'id':
+            result['id'] = str(value)
+        attributes[str(key)] = value
+    result['attributes'] = attributes
+    return result
--- kujira/rest/lib/parsing_methods.py
+++ kujira/rest/lib/parsing_methods.py
@@ -1,7 +1,6 @@
 """Library which contains helpers used in JSON API parsing process.
 Helpers defined:
-- parse_and_return - for parsing response to JSON API format
-                using method defined in appropriate controller
+- parse_and_return - for parsing response to JSON API format using method defined in appropriate controller
 - create_error_422 - creating error 422 packed to JSON API format"""
 
 import json
@@ -14,8 +13,7 @@ def parse_and_return(method, response):
     json_response = json.dumps(response)
     json_dict = json.loads(json_response)
     output = method(json_dict)
-    json_output = Response(json.dumps(output, indent=2),
-                           content_type='application/json')
+    json_output = Response(json.dumps(output, indent=2), content_type='application/json')
     return json_output
 
 
@@ -24,6 +22,5 @@ def create_error_422(source, message):
     errors = {'errors': [{'status': '422'}]}
     errors['errors'][0]['source'] = str(source)
     errors['errors'][0]['details'] = message
-    json_errors = Response(json.dumps(errors, indent=2),
-                           content_type="application/vnd.api+json", status=422)
+    json_errors = Response(json.dumps(errors, indent=2), content_type="application/vnd.api+json", status=422)
     return json_errors
