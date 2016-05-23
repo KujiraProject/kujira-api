a8cb660fbb58dd7a4c9a718a72bdbb49cb3783c6
Szprync <gleamus@gmail.com>
2016-05-23 13:59:36 +0100

    Logger path fix

--- kujira/websocket/__init__.py
+++ kujira/websocket/__init__.py
@@ -4,13 +4,22 @@ Websocket package
 Handles server-client communications: event and graph data
 """
 import logging
+import os
+import errno
+
+LOGGER_FILE_PATH = '/var/log/kujira/websocket.log'
+
+# Create directory if needed
+if not os.path.exists(os.path.dirname(LOGGER_FILE_PATH)):
+    os.makedirs(os.path.dirname(LOGGER_FILE_PATH))
 
 LOGGER = logging.getLogger(__name__)
 LOGGER.setLevel(logging.DEBUG)
 
+HANDLER = logging.FileHandler(LOGGER_FILE_PATH, 'w')
+
 FORMATTER = logging.Formatter(
     '%(asctime)s %(threadName)-11s %(name)-9s %(message)s')
-HANDLER = logging.FileHandler('/var/log/websocket.log', 'w')
 HANDLER.setFormatter(FORMATTER)
 
 LOGGER.addHandler(HANDLER)
