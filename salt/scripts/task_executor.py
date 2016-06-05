e84e43088912b4711443cdd715ce28f693fcef3d
arthurn1910 <radekpawlaczyk@gmail.com>
2016-06-05 16:08:46 +0200

    refactoring code to execute single and parallel tasks

--- salt/scripts/task_executor.py
+++ salt/scripts/task_executor.py
@@ -14,8 +14,7 @@ FORMATTER = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 HANDLER.setFormatter(FORMATTER)
 LOG.addHandler(HANDLER)
 
-FETCH_TIME = 5 # interval to fetch next task
-CHECK_TIME = 2 # interval to check if task finished
+FETCH_TIME = 1 #10# interval to fetch next task
 
 try:
     import TaskStorage
@@ -25,13 +24,24 @@ except ImportError:
     class TaskStorage(object):
         def __init__(self):
             self.nt = 0
-            self.tasks = [[{"host": "mng",   "module": 'test.sleep', "arg": "3",   "jid": "", "status": ""}],
-                          [{}],  # It means that there isnt any task at the moment
-                          [{}],  # It means that there isnt any task at the moment
-                          [{"host": "node1",   "module": 'cmd.run',    "arg": "pepe", "jid": "", "status": ""},
-                          {"host": "node1", "module": 'test.sleep', "arg": "1",    "jid": "", "status": ""}],
-                          [{}],  # It means that there isnt any task at the moment
-                          [{"host": "mng",   "module": 'cmd.run',    "arg": "ls",   "jid": "", "status": ""}]]
+            self.tasks = [{}, #empty task
+                          {
+                                "parallel": False,
+                                "title": "Add node5 to Ceph",
+                                "subtask":[{"host": "mng",   "module": 'test.sleep', "arg": "5",   "jid": "", "status": ""},
+                                    {"host": "mng",   "module": 'test.sleep', "arg": "5",   "jid": "", "status": ""},
+                                    {"host": "mng",   "module": 'test.sleep', "arg": "5",   "jid": "", "status": ""}
+                                ],
+                          },
+                          {
+                                "parallel": True,
+                                "title": "Add node5 to Ceph",
+                                "subtask":[{"host": "mng",   "module": 'test.sleep', "arg": "40",   "jid": "", "status": ""},
+                                    {"host": "mng",   "module": 'test.sleep', "arg": "40",   "jid": "", "status": ""},
+                                    {"host": "mng",   "module": 'test.sleep', "arg": "40",   "jid": "", "status": ""}
+                                ],
+                          }
+                         ]
 
         def get_task(self):
             if self.nt >= len(self.tasks):
@@ -42,71 +52,91 @@ except ImportError:
             return task
 
         def update(self, task):
-            return
+            self.tasks[self.nt - 1] = task
+            LOG.debug("Updating task: %s", task)
 
-class get_task(object):
-    """Class connect to database and take list with tasks to execute and return task"""
-    def __init__(self):
-        self.tasks = TaskStorage().get_task()
-        self.connection = TaskStorage()
-        
-    def get_task(self):
-        """Function which return task to execute"""
-        if len(self.tasks)==0:
-            self.tasks = self.connection.get_task()
-        if len(self.tasks)!=0:
-            return self.tasks.pop()
-        return
-
-def execute(task):
+def execute(subtask):
     """Function which execute tasks"""
-    LOG.info("Executing task: %s", task)
+    LOG.info("Executing subtask: %s", subtask)
     client_salt = salt.client.LocalClient()
-    job_id = client_salt.cmd_async(task["host"], task["module"], [task["arg"]])
+    job_id = client_salt.cmd_async(subtask["host"], subtask["module"], [subtask["arg"]])
     return job_id
 
-def wait_for_finish(task):
+def finish(subtask):
     """Function which wait for task status"""
     client_salt = salt.client.LocalClient()
     while True:
         try:
-            ret_temp = client_salt.get_cli_returns(task['jid'], task['host'])
+            ret_temp = client_salt.get_cli_returns(subtask['jid'], subtask['host'])
             ret = [x for x in ret_temp]
             if ret:
-                if ret[0][task['host']]['ret'] == True:
-                    LOG.info('Task finished. JID: %s', task['jid'])
-                    return True
+                if ret[0][subtask['host']]['ret'] == True:
+                    LOG.info('Subtask finished. JID: %s', subtask['jid'])
+                    subtask['status'] = 'Finished'
+                    return subtask
                 else:
-                    LOG.info('Task failed. JID: %s', task['jid'])
-                    return False
+                    LOG.info('Subtask failed. JID: %s', subtask['jid'])
+                    subtask['status'] = 'Failed'
+                    return subtask
             else:
-                LOG.info('Task still running. JID: %s', task['jid'])
-            time.sleep(CHECK_TIME)
+                LOG.info('Subtask still running. JID: %s', subtask['jid'])
+                return False
         except KeyError:
             continue # Check it
 
+def execute_parallel(connection, task):
+    jids = []
+    #Executions 
+    list_subtasks=task['subtask'] #take list of subtask from task
+    for i in range(0, len(list_subtasks)):
+        subtask = list_subtasks[i]
+        jid = execute(subtask)
+        subtask['jid'] = jid
+        subtask['status'] = 'Executing'
+        jids.append(jid)
+        list_subtasks[i] = subtask
+    task['subtask']=list_subtasks
+    connection.update(task)
+    
+    #Check
+    while jids:
+        for subtask in list_subtasks:
+            if not subtask['jid'] in jids:
+                continue
+            if finish(subtask):
+                jids.remove(subtask['jid'])
+    connection.update(task)
+    
+def execute_single(connection, task):
+    #Executions 
+    list_subtasks=task['subtask'] #take list of subtask from task
+    for i in range(0, len(list_subtasks)):
+        jid = execute(list_subtasks[i])
+        list_subtasks[i]['jid'] = jid
+        list_subtasks[i]['status'] = 'Executing'
+        task['subtask']=list_subtasks
+        connection.update(task)
+        while True:
+            tmp=finish(list_subtasks[i])
+            if tmp:
+                list_subtasks[i]=tmp
+                break
+        task['subtask']=list_subtasks
+        connection.update(task)
+
 def start():
     """Main function where take and execute tasks """
-    connect = get_task()
+    #connect = Configurate_task()
     connection = TaskStorage()
     while True:
-        task = connect.get_task()
+        task = connection.get_task()
         if not task:
             LOG.debug("Waiting %d seconds to ask for next task...", FETCH_TIME)
             time.sleep(FETCH_TIME)  # sleep asking for next task
             continue
-        jid = execute(task)
-        task['jid'] = jid
-        task['status'] = 'Executing'
-        connection.update(task)
-        if jid == 0:
-            task['status'] = 'Error'
-            connection.update(task)
-            continue
-        if wait_for_finish(task):
-            task['status'] = 'Finished'
+        LOG.info("Fetch task: {}".format(task))
+        if(task['parallel']==True):
+            execute_parallel(connection,task)
         else:
-            task['status'] = 'Failed'
-        connection.update(task)
-        
+            execute_single(connection, task)
 start()
