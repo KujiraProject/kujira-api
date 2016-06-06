2dd7846d916d8446cafd63aab73f5ff9f0c1200a
Kacper Kołodziej <kacper@kolodziej.in>
2016-06-06 23:29:50 +0200

    add some data to Plugin and adjust osd plugins
    Each plugin has now name field which describes it.
    Osd plugins are adjusted to use it. Abstract Plugin
    has also data function which returns dictionary with
    all information about event.

--- kujira/scheduler/plugins/osd/remove.py
+++ kujira/scheduler/plugins/osd/remove.py
@@ -3,22 +3,25 @@
 from kujira.scheduler.plugins.plugin import Plugin
 
 class Remove(Plugin):
-    
+    name = 'osd.remove'
+
     def is_valid(self):
         return (True, None)
 
     def can_run(self):
-        if not self.mongo_check():
+        if not self.check_if_exists():
             return (False, "Mongo check failed.")
         return (True, None)
 
     def ceph_query(self):
         pass
         
-    def mongo_check(self):
+    def check_if_exists(self):
         tasks = self.mongo.get_all_tasks()
         
         for task in tasks:
-            if task["title"] == "osd.remove" and task["arg"] == self.params["arg"]:
+            if (task["title"] == self.name and
+               task["arg"] == self.params["arg"]):
                 return False
+
         return True
--- kujira/scheduler/plugins/plugin.py
+++ kujira/scheduler/plugins/plugin.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from kujira.store.tasks import Mongodb
+from datetime import datetime
 
 class Plugin(object):
-    
+
     def __init__(self, **params):
+        self.create_date = datetime.now()
         self.params = params
         self.mongo = Mongodb()
         self.mongo.connect("mydb", "tasks", "oldTasks")
@@ -14,3 +16,10 @@ class Plugin(object):
 
     def can_run(self):
         raise NotImplementedError("Plugin.can_run must be implemented!")
+
+    def data(self):
+        return {
+            'name': self.name,
+            'date': self.create_date.strftime('%c'),
+            'params': self.params,
+        }
