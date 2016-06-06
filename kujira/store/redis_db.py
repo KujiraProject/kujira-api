2ea6f54668e3000252ebba9e237cf123d62c7686
tkorpowski <tk.code.github@gmail.com>
2016-06-07 00:23:41 +0200

    RedisConnection.is_connected() now returns False
    is_connected() returns False instead of rising exception when not connected

--- kujira/store/redis_db.py
+++ kujira/store/redis_db.py
@@ -20,4 +20,4 @@ class RedisConnection(object):
         try:
             return bool(self.connection.ping())
         except redis.ConnectionError:
-            raise exceptions.ConnectionError('Cannot connect to database!')
+            return False
