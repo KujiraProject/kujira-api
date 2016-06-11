62bc946f92e650365d045b7653120a73bacb8b33
Kacper Kołodziej <kacper@kolodziej.in>
2016-06-11 20:22:17 +0200

    add logger config and fix codestyle problems

--- kujira/scheduler/__init__.py
+++ kujira/scheduler/__init__.py
@@ -1,2 +1,23 @@
-from scheduler import Scheduler
-from . import plugins
+#  -*- coding: utf-8 -*-
+"""Tasks' scheduler for Kujira"""
+
+import logging
+import os
+from kujira.scheduler.scheduler import Scheduler
+from kujira.scheduler import plugins
+
+LOG_FILE_PATH = '/var/log/kujira/scheduler.log'
+
+# Create directory for logs if not exists
+if not os.path.exists(os.path.dirname(LOG_FILE_PATH)):
+    os.makedirs(os.path.dirname(LOG_FILE_PATH))
+
+LOG = logging.getLogger(__name__)
+
+LOG_FILE = logging.FileHandler(LOG_FILE_PATH, 'w+')
+
+FORMATTER = logging.Formatter(
+    '%(asctime)s %(threadName)s %(name)s: %(message)s')
+LOG_FILE.setFormatter(FORMATTER)
+
+LOG.addHandler(LOG_FILE)
