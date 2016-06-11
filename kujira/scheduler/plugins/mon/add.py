5aadd285f450b3b1a434c34e9de9b6a4e63110a1
Kacper Kołodziej <kacper@kolodziej.in>
2016-06-11 22:03:45 +0200

    add monitor plugins

---
+++ kujira/scheduler/plugins/mon/add.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""This plugin describes task of adding a monitor to cluster"""
+
+from kujira.scheduler.plugins.plugin import Plugin
+
+class Add(Plugin):
+    """Add a monitor to cluster"""
+    salt_module_name = 'kujira.mon.add'
+
+    def is_valid(self):
+        if 'host' not in self.params:
+            return (False, "'host' param is required!")
+
+        return (True, None)
+
+    def can_run(self):
+        if not self.check_if_exists():
+            return (False, "Adding a monitor on {0} " \
+            "is already in queue!".format(self.params['host']))
+
+        return (True, None)
+        
+    def title(self):
+        return "Add MON on node {node}".format(node=self.params['host'])
+
