2d8c86cbe01a17de2c819f80c4deda6e886abc02
Kacper Kołodziej <kacper@kolodziej.in>
2016-06-14 09:14:22 +0200

    add monitor plugins

--- kujira/scheduler/plugins/config.py
+++ kujira/scheduler/plugins/config.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 """Map plugin's names to types"""
 
-from kujira.scheduler.plugins.osd.add import Add
-from kujira.scheduler.plugins.osd.remove import Remove
+from kujira.scheduler.plugins import osd
+from kujira.scheduler.plugins import mon
 
 PLUGINS = {
-    "osd.add": Add,
-    "osd.remove": Remove
+    "osd.add": osd.Add,
+    "osd.remove": osd.Remove,
+    "mon.add": mon.Add,
+    "mon.remove": mon.Remove,
 }
