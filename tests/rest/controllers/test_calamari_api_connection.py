a1d4ea44ec1466e5ed442aff3abb844694247ab3
vmyrhorodskyi <vmyrhorodskyi@gmail.com>
2016-06-15 12:08:14 +0200

    test for Calamari API connection with docstrings

--- tests/rest/controllers/test_calamari_api_connection.py
+++ tests/rest/controllers/test_calamari_api_connection.py
@@ -7,21 +7,31 @@ from kujira.rest.lib.calamari_client import CalamariClient
 from kujira.rest.lib.parsing_methods import create_error_422
 
 
-
 class CalamariAPIConnectionTestCase(unittest.TestCase):
+    """Calamari API connection test case"""
 
     def test_server_is_up_and_running(self):
-        constr = CalamariClient(api_url=CALAMARI_API_URL, username=CALAMARI_API_USER,
-                                password=CALAMARI_API_PWD, timeout=CALAMARI_API_TIMEOUT)
+        """ Calamari API request test:
+            expected response status code - 200
+        """
+        constr = CalamariClient(api_url=CALAMARI_API_URL,
+                                username=CALAMARI_API_USER,
+                                password=CALAMARI_API_PWD,
+                                timeout=CALAMARI_API_TIMEOUT)
         client = constr.authenticate()
         response = client.get(constr._api_url, timeout=constr._timeout)
         self.assertEqual(response.status_code, 200)
 
 
     def test_error_422(self):
+        """ Testing request with wrong Calamari API url:
+            422 error is expected
+        """
         try:
-            constr = CalamariClient(api_url="http://localhost/api/v2/", username=CALAMARI_API_USER,
-                                password=CALAMARI_API_PWD, timeout=CALAMARI_API_TIMEOUT)
+            constr = CalamariClient(api_url="http://wrong_calamari_api_url/api/v2/",
+                                    username=CALAMARI_API_USER,
+                                    password=CALAMARI_API_PWD,
+                                    timeout=CALAMARI_API_TIMEOUT)
             client = constr.authenticate()
             response = client.get(constr._api_url, timeout=constr._timeout)
         except ConnectionError as err:
@@ -29,6 +39,5 @@ class CalamariAPIConnectionTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 422)
 
 
-
 if __name__ == '__main__':
-    unittest.main();
+    unittest.main()
